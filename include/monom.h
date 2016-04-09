19b91c11c3dbca8b5b1169fa4805fe7ecd3023b5
Alexey Dubov <alduxd@gmail.com>
2016-04-09 17:27:56 +0300

    class monom

---
+++ include/monom.h
@@ -0,0 +1,33 @@
+﻿#ifndef _MONOM_H_
+#define _MONOM_H_
+#include <iostream>
+#include <sstream>
+#include <locale>
+
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::istringstream;
+using std::istream;
+using std::ostream;
+
+class Monom
+{
+	double coeff;
+    int deg;
+	Monom *next;
+public:
+    Monom(int = 0, int = 0, Monom* = NULL);
+	Monom(const Monom &m);
+	void SetCoeff(int coeff);
+    int GetCoeff();
+	void SetDegree(int deg);
+    int GetDegree();
+	void SetNext(Monom *_next);
+	Monom* GetNext();
+	Monom& operator=(const Monom &m);
+	bool operator==(const Monom &m) const;
+};
+
+#endif
